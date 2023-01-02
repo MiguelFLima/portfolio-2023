9e74d66556f6c3a7e6a8e5a769245a50491374c4
Miguel <miguel2014ssa@gmail.com>
2023-01-02 15:47:41 -0300

    projects's page

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import Loader from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters";
+import portfolioData from "../../data/Portfolio.json";
+import "./index.scss";
+
+const Portfolio = () => {
+  const [letterClass, setLettersClass] = useState("text-animate");
+
+  console.log("portfolioData", portfolioData);
+  useEffect(() => {
+    setTimeout(() => {
+      setLettersClass("text-animate-hover");
+    }, 4000);
+  }, []);
+
+  const renderPortfolio = (portfolio) => {
+    return (
+      <div className="images-container">
+        {portfolio.map((project, idx) => (
+          // I know key with idx its  bad practice...
+          <div className="image-box" key={idx}>
+            <img
+              className="portfolio-image"
+              src={project.cover}
+              alt="project's cover"
+            />
+            <div className="content">
+              <p className="title">{project.title}</p>
+              <h4 className="description">{project.description}</h4>
+              <button onClick={() => window.open(project.url)} className="btn">
+                View
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <div className="container portfolio-page">
+        <h1 className="page-title">
+          <AnimatedLetters
+            idx={15}
+            strArray={"Portfólio".split("")}
+            letterClass={letterClass}
+          />
+        </h1>
+        <div>{renderPortfolio(portfolioData.portfolio)}</div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  );
+};
+
+export default Portfolio;
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,9 +1,12 @@
 import { Link, NavLink } from "react-router-dom";
 import "./index.scss";
-import LogoS from "../../assets/images/logo-s.png";
-import LogoSubtitle from "../../assets/images/logo_sub.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faEnvelope,
+  faHome,
+  faSuitcase,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Sidebar = () => {
@@ -22,7 +25,13 @@ const Sidebar = () => {
           <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
         </NavLink>
         <NavLink
-          exact="true"
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
           activeclassname="active"
           className="contact-link"
           to="/contact"
